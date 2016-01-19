eea25e0bd473c0567119b214ad59b43ea764cde4
Hamish Hutchings <hamhut1066@gmail.com>
2016-01-19 18:09:24 +0000

    add a prompt

--- scripts/console.py
+++ scripts/console.py
@@ -8,16 +8,17 @@ import time
 
 
 def constructConsole():
-  console = Serial(port="/dev/ttyACM1", timeout=0)
+  console = Serial(port="/dev/ttyACM0", timeout=0)
   return console
 
 def main():
   console = constructConsole()
   print("Write commands here:")
   while True:
+    print ">",
     input = raw_input()
     console.write(input)
-    time.sleep(0.2)
+    time.sleep(1.2)
     print(console.readline())
 
 if __name__ == "__main__":
