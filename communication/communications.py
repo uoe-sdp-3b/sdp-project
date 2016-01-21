bfabed58f12387024eb3ce622d82e41da5ba6a86
Hamish Hutchings <hamhut1066@gmail.com>
2016-01-21 15:22:49 +0000

    Move system to use queue system.
    - Extract out Constants into the root of the file.
    - explicitly cast all command inputs to integers.

--- communication/communications.py
+++ communication/communications.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
 from serial import Serial
+from Queue import Queue
 import fileinput
 import readline
 from time import sleep
 from threading import Thread
 
+# constants
+TEAM = 0
+
+# Operation Codes (OpCodes)
+STOP = 0
+FORWARD = 1
+BACKWARD = 2
+LEFT = 3
+RIGHT = 4
+GRAB = 5
+
+
+
 class CommsToArduino(object):
+
+    queue = Queue()
+    internal_queue = Queue()
+    write_queue = Queue()
     # these should be hard-coded, the values should not change
     def __init__(self, port="/dev/ttyACM0",
                  rate=115200,
@@ -17,7 +35,7 @@ class CommsToArduino(object):
         self.rate = rate
         self.timeout = timeout
         self.connect()
-    
+
     seqNo = False
     ready = True
 
@@ -33,12 +51,12 @@ class CommsToArduino(object):
             except OSError as ex:
                 print("Cannot connect to Arduino.")
                 print(ex.strerror)
-                
+
     def create_checksum(self, arg, opcode):
         """
         Creates the checksum that is used to verify the opcode.
         """
-        
+
         return (arg + opcode) % 10
 
     def _write(self, sig, opcode, arg, seqNo):
@@ -80,44 +98,75 @@ class CommsToArduino(object):
         if self.isConnected:
             self.ready = False
             self.seqNo = not self.seqNo
-            
-            thread = Thread(target = self._write, args = (sig, opcode, arg, self.seqNo))
-            thread.start()
+
+            self.write_queue.put({
+                'sig': sig,
+                'opcode': opcode,
+                'arg': arg,
+                'seqNo': self.seqNo
+            })
         else:
             print("Not connected to Arduino.")
 
 # plan on keeping this as a skeleton used purely for communication
 class RobotComms(CommsToArduino):
-  TEAM = 0
-  
-  # Operation Codes (OpCodes)
-  STOP = 0
-  FORWARD = 1
-  BACKWARD = 2
-  LEFT = 3
-  RIGHT = 4
-  GRAB = 5
-  
+  _close = False
+
   def __init__(self, port):
-    super(RobotComms, self).__init__(port)
+      self.write_thread = Thread(target = self.write_stream)
+      self.read_thread = Thread(target = self.read_stream)
+      self.read_thread.start()
+      self.write_thread.start()
+      super(RobotComms, self).__init__(port)
+
+  def close(self):
+      self._close = True
+      self.comn.close()
+      self.queue.put("Robot Closed")
+
+
+  def write_stream(self):
+      while True:
+          if self._close:
+              self.queue.put("Read Stream Closed")
+              break
+          if not self.write_queue.empty():
+              msg_dict = self.write_queue.get()
+              self._write(
+                  msg_dict["sig"],
+                  msg_dict["opcode"],
+                  msg_dict["arg"],
+                  msg_dict["seqNo"]
+              )
+
+  def read_stream(self):
+      while True:
+          if self._close:
+              self.queue.put("Read Stream Closed")
+              break
+          sleep(0.5)
+          if self.comn and self.comn.is_open:
+              line = self.comn.readline()
+              if line.strip() != "":
+                  self.queue.put(line)
 
   def stop(self):
-    self.write(TEAM, STOP, 0)
+      self.write(TEAM, STOP, 0)
 
   def forward(self, speed):
-    self.write(TEAM, FORWARD, speed)
+      self.write(TEAM, FORWARD, int(speed))
 
   def backward(self, speed):
-    self.write(TEAM, BACKWARD, speed)
+      self.write(TEAM, BACKWARD, int(speed))
 
   def left(self, speed):
-    self.write(TEAM, LEFT, speed)
+      self.write(TEAM, LEFT, int(speed))
 
   def right(self, speed):
-    self.write(TEAM, RIGHT, speed)
+      self.write(TEAM, RIGHT, int(speed))
 
   def grab(self, speed):
-    self.write(TEAM, GRAB, speed)
+      self.write(TEAM, GRAB, speed)
 
 if __name__ == "__main__":
-  print("This class is not designed to be run by hand")
+    print("This class is not designed to be run by hand")
