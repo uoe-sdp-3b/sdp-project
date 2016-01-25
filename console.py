0ad01a1c78440d695401c6dc4de8a48da535bbba
Justas Zemgulys <s1353182@sms.ed.ac.uk>
2016-01-25 18:41:32 +0000

    Revert "Debug and prepare python for sending over file"

--- console.py
+++ console.py
@@ -2,43 +2,45 @@
 from communication.communications import RobotComms
 from ncurses.cursesUI import NcursesUI
 
+
 class Console():
-  def __init__(self, port):
-    self.robot = RobotComms(port)
-    self.ui = NcursesUI(self.read, self.write)
+    def __init__(self, port):
+        self.robot = RobotComms(port)
+        self.ui = NcursesUI(self.read, self.write)
 
-  def start(self):
-    self.robot.connect()
-    self.ui.start()
+    def start(self):
+        self.robot.connect()
+        self.ui.start()
 
-  def read(self):
-    if not self.robot.queue.empty():
-      message = self.robot.queue.get()
-      return message.strip()
-    return None
+    def read(self):
+        if not self.robot.queue.empty():
+            message = self.robot.queue.get()
+            return message.strip()
+        return None
 
-  def write(self, message):
-    args = message.split(" ")
-    f = getattr(self.robot, args[0], None)
+    def write(self, message):
+        args = message.split(" ")
+        f = getattr(self.robot, args[0], None)
 
-    try:
-      if f and len(args) > 1:
-        f(*args[1:])
-      elif f:
-        f()
-    except TypeError as e:
-      self.robot.queue.put(str(e))
+        try:
+            if f and len(args) > 1:
+                f(*args[1:])
+            elif f:
+                f()
+        except TypeError as e:
+            self.robot.queue.put(str(e))
+
 
 def main():
-  import argparse
-  parser = argparse.ArgumentParser(description="console for robot")
-  parser.add_argument("-p", default="/dev/ttyACM0")
+    import argparse
+    parser = argparse.ArgumentParser(description="console for robot")
+    parser.add_argument("-p", default="/dev/ttyACM0")
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  console = Console(args.p)
-  console.start()
+    console = Console(args.p)
+    console.start()
 
 
 if __name__ == "__main__":
-  main()
+    main()
