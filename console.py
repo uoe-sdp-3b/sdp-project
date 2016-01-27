0f183b48e1395bbda204c3b6bbf183284ae4117d
Hamish Hutchings <hamhut1066@gmail.com>
2016-01-27 00:48:50 +0000

    cleanup console code

--- console.py
+++ console.py
@@ -16,9 +16,14 @@ class Console():
         if not self.robot.queue.empty():
             message = self.robot.queue.get()
             return message.strip()
+            if message:
+                return message.strip()
         return None
 
     def write(self, message):
+        if message == "":
+            return
+
         args = message.split(" ")
         f = getattr(self.robot, args[0], None)
 
@@ -27,6 +32,8 @@ class Console():
                 f(*args[1:])
             elif f:
                 f()
+            else:
+                self.robot.queue.put("argument: '%s' doesn't exist" % args[0])
         except TypeError as e:
             self.robot.queue.put(str(e))
 
