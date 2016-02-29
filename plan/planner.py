a4154d3a5c5644c2fb3807baaac3ef3e929aaa50
madspark <berniukstis@gmail.com>
2016-02-29 20:08:51 +0000

    Finish planner prototype v1 (untested)

--- plan/planner.py
+++ plan/planner.py
@@ -131,9 +131,9 @@ class Planner(object):
     # CORE functions
 
     def get_ball(self):
+        self.clear_robot_responses()
         while True:
             command = ""
-            # (ball_coordinates, robot_coordinates, robot_dir_vector) = get_info(self.camera)
 
             v1 =  self.world[self.us]["location"]      # our robot's coordinates
             v2 =  self.world["ball_center"] # ball's coordinates
@@ -148,28 +148,37 @@ class Planner(object):
                 break
             else:
                 command += "forward " + str(int(math.ceil(0.7 * distance)))  # * 0.8
-                # self.compose(command)
-
-                # !! can I override the last command the bot got or not?
-                # If *I can't*
-                # then sleep(5)
-
+                robot.compose(command)
+                
+                # !! Can be written differently if can interrupt robot's previous command
+                # !! Can check for response == success
+                self.wait_for_robot_response()
+                self.clear_robot_responses()
+                
+                # Alternative: sleep(5)
+        
+        command = ""
         command += "open $"
         command += "forward " + str(int(math.ceil(distance))) + " $"
         command += "stop"
-        # self.compose(command)
-
-        # check with IR if ball got.
-        # IF NOT GOT:
-        # get_ball()
+        
+        robot.compose(command)  
+        
+        self.wait_for_robot_response()
+        
+        # !! Should check if ball was got with IR (or vision)
+        # !! Call get_ball() recursively if not
+        
+        self.clear_robot_responses()
 
     def get_to(self, location):
+        self.clear_robot_responses()
         while True:
             command = ""
-            # (robot_coordinates, robot_dir_vector) = get_info(self.camera)
 
-            v1 = robot_coordinates
-            v2 = location
+            v1 =  self.world[self.us]["location"] # our robot's coordinates
+            v2 =  location
+            robot_dir_vector = self.world[self.us]["orientation"]
 
             turn = self.angle_a_to_b(v1, v2, robot_dir_vector)
             command += self.turn_command(turn) + " $"
@@ -180,82 +189,87 @@ class Planner(object):
                 break
             else:
                 command += "forward " + str(int(math.ceil(0.7 * distance)))  # * 0.8
-                # self.compose(command)
+                robot.compose(command)
 
-                # !! can I override the last command the bot got or not?
-                # If *I can't*
-                # then sleep(5)
+                # !! Can be written differently if can interrupt robot's previous command
+                # !! Can check for response == success
+                self.wait_for_robot_response()
+                self.clear_robot_responses()
 
-        command += "forward " + str(int(math.ceil(distance)))
-        # self.compose(command)
+        command = "forward " + str(int(math.ceil(distance)))
+        robot.compose(command)  
+        
+        self.wait_for_robot_response()
+        self.clear_robot_responses()
 
     # PART 1
     def receive_pass(self):
-        # (ball_coordinates, robot_coordinates, robot_dir_vector) = get_info(self.camera)
-
-        # !! can make robot face goal, ball or own team plate initially, matters not
-        (ball_coordinates, robot_coordinates, robot_dir_vector) = ((-320*0.46, 0), (50, 60), (1, 0))
-
-        v1 = robot_coordinates
-        v2 = ball_coordinates
+    
+        v1 =  self.world[self.us]["location"]      # our robot's coordinates
+        v2 =  self.world["ball_center"] # ball's coordinates
+        robot_dir_vector = self.world[self.us]["orientation"]
 
         turn = self.angle_a_to_b(v1, v2, robot_dir_vector)
-
-        # comms.compose(self.turn_command(turn))
+        command = self.turn_command(turn)
+        
+        self.robot.compose(command)
 
         # !! maybe can wait a bit here
-        # CHASE AND GET BALL FUNCTION
+        self.get_ball()
 
         log.debug(command)
-        # self.compose(command)
 
     # PART 2
     def receive_turn_pass(self):
-        # (ball_coordinates, robot_coordinates, robot_dir_vector) = get_info(self.camera)
-
-        # !! can make robot face enemy plate or ball, matters not
-        (ball_coordinates, robot_coordinates, robot_dir_vector) = ((-20, -40), (-320*0.46, 0), (1, 0))
-
-        v1 = robot_coordinates
-        v2 = ball_coordinates
+    
+        v1 =  self.world[self.us]["location"]s
+        v2 =  self.world["ball_center"]
+        robot_dir_vector = self.world[self.us]["orientation"]
 
         turn = self.angle_a_to_b(v1, v2, robot_dir_vector)
 
-        # comms.compose(self.turn_command(turn))
+        robot.compose(self.turn_command(turn))
 
         # !! maybe can wait a bit here
-        # CHASE AND GET BALL FUNCTION
+        self.get_ball()
 
-        (teammate_location, robot_coordinates, robot_dir_vector) = ((50, 60), (-320*0.46, 0), (1, -1))
-
-        v1 = robot_coordinates
-        v2 = teammate_coordinates
+        v1 =  self.world[self.us]["location"]
+        v2 =  self.world[self.ally]["location"]
+        robot_dir_vector = self.world[self.us]["orientation"]
 
         turn = self.angle_a_to_b(v1, v2, robot_dir_vector)
-
-        # !! Can confirm with camera?
-        # !! Can ask camera or compass to confirm shit
-        # comms.compose(self.turn_command(turn))
-
-        # comms.compose("kick 100")
-
+        
+        # !! can use one command instead
+        
+        command = ""
+        command += self.turn_command(turn) + " $ "
+        command += self.turn_command("stop") + " $ "
+        command += "kick 100"
+        
+        self.robot.compose(command)
 
         log.debug(command)
-        # self.compose(command)
 
     # PART 3
     def intercept(self):
         # (ball_coordinates, robot_coordinates, robot_dir_vector) = get_info(self.camera)
-
-        # !! can make robot face enemy plate or ball, matters not
-        # !! plz correctly distinguish which enemy is which
-        (ball_coordinates, other_bot, robot_coordinates) = ( (320*0.46,0), (-40,100), (40,-100), (1,0) )
+        robot_coordinates =  self.world[self.us]["location"]
+        green_opp = self.world["green_opponent"]["location"]
+        pink_opp = self.world["pink_opponent"]["location"]
+        
+        ball_coordinates =  self.world["ball_center"]
+        
+        # Choose the opponent which is not near the ball
+        if self.dist(ball_coordinates, green_opp) > self.dist(ball_coordinates, pink_opp):
+            other_bot = green_opp
+        else:
+            other_bot = pink_opp
 
         # Solving geometric problem with x,y axes for i_location
         # y = kx + m
 
         # k = (y2-y1) / (x2 - x1)
-        k = (other_bot[1] - ball_coordinates[1]) / (other_bot[0] - ball_coordinates[0])
+        k = (ball_coordinates[1] - other_bot[1) / (ball_coordinates[0] - other_bot[0])
 
         # m = y - kx
         m = other_bot[1] - k * other_bot[0]
@@ -268,20 +282,28 @@ class Planner(object):
 
 
     def defend(self):
-        #(ball_coordinates, robot_coordinates, robot_dir_vector) = get_info(self.camera)
-
         # !! this is simplest possible strategy
         # !! can change to robot seeking ball within confined defence area
 
         (enemy_bot, robot_coordinates) = ( (320*0.46,0), (-40,100), (40,-100), (1,0) )
-
+        
+        robot_coordinates =  self.world[self.us]["location"]
+        green_opp = self.world["green_opponent"]["location"]
+        pink_opp = self.world["pink_opponent"]["location"]
+        
+        # !! Not sure if this works
+        if green_opp is not None:
+            enemy_bot = green_opp
+        else:
+            enemy_bot = pink_opp
+        
         goal_center = (320*0.46,0)
 
         # Solving geometric problem with x,y axes for i_location
         # y = kx + m
 
         # k = (y2-y1) / (x2 - x1)
-        k = (enemy_bot[1] - goal_center[1]) / (enemy_bot[0] - goal_center[0])
+        k = (goal_center[1] - enemy_bot[1]) / (goal_center[0] - enemy_bot[0])
 
         # m = y - kx
         m = enemy_bot[1] - k * enemy_bot[0]
@@ -304,6 +326,15 @@ class Planner(object):
 
     def dist(self, v1, v2):
         return math.hypot(v1[0] - v2[0], v1[1] - v2[1])
+        
+    def wait_for_robot_response():
+        while(self.robot.queue.empty()):
+            pass
+    
+    def clear_robot_responses():
+        # Empty response queue
+        while(not self.robot.queue.empty()):
+            self.robot.queue.get()
 
     # Returns string "[left/right]  [turn degrees (<=180)]"
     def turn_command(self, turn):
